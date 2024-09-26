1dcfbcfa69cd35157dda4a1dbf34a6cb571d8cd4
Imaad-Daniels2903 <127976033+Imaad-Daniels2903@users.noreply.github.com>
2024-09-26 20:20:29 +0200

    ZigZag

---
+++ main.py
@@ -0,0 +1,10 @@
+import math
+
+# text_in = input('enter text')
+# text_len = len(text_in)
+# text_line = ([(['']*4)] * text_len)
+# print(text_line)
+
+test_list = []
+test_list.append(['money'])
+print(test_list[0][0])
